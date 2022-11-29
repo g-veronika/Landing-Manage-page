6aaa871b6a5c62c1d9196124223c26f9840172e8
g-veronika <g.veronika2021@gmail.com>
2022-11-29 15:03:39 +0100

    hover on menu elements

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,7 @@ module.exports = {
       xl: '1440xp'
     },
     extend: {
-      color: {
+      colors: {
         brightRed: 'hsl(12, 88%, 59%)',
         brightRedLight: 'hsl(12, 88%, 69%)',
         brightRedSupLight: 'hsl(12, 88%, 95%)',
@@ -17,8 +17,8 @@ module.exports = {
         darkGrayishBlue: 'hsl(227, 12%, 61%)',
         veryDarkBlue: 'hsl(233, 12%, 13%)',
         veryPaleRed: 'hsl(13, 100%, 96%)',
-        veryLightGray: 'hsl(0, 0%, 98%)'
-      }
+        veryLightGray: 'hsl(0, 0%, 98%)',
+      },
     },
   },
   plugins: [],
